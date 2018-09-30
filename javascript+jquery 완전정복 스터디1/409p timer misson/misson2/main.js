cc0ed08dacbbc1d162d6ed0acaf9bab64e1273a4
dohyun8736 <dohyun8736@naver.com>
2018-09-30 23:07:40 +0900

    timer misson2 수정

--- javascript+jquery 완전정복 스터디1/409p timer misson/misson2/main.js
+++ javascript+jquery 완전정복 스터디1/409p timer misson/misson2/main.js
@@ -1,53 +1,54 @@
 var fish = null;
-var step = 50;
-var timerId = 0;
+var move = 50;
+var timerID = 0;
+
 $(document).ready(function(){
-    init();
-    initEvent();
+    fishInit();
+    fishEvent();
 });
 
-function init(){
+function fishInit(){
     fish = $("#fish");
 }
 
-function initEvent(){
+function fishMove(){
+    var x = fish.position().left + move;
 
+    if(x >= 430){
+        x = 430;
+        fish.attr("src", "fish2.png");
+        move = -50;
+    }
+    if(x <= 50){
+        x = 50;
+        fish.attr("src", "fish1.png");
+        move = 50;
+    }
+
+    fish.css("left", x);
+}
+
+function start(){
+    console.log(timerID);
+    if(timerID == 0){
+        timerID = setInterval(function(){
+            fishMove();
+        },100);
+        console.log(timerID);
+    }
+}
+
+function stop(){
+    clearInterval(timerID);
+    timerID = 0;
+}
+
+function fishEvent(){
     $("#btnStart").click(function(){
         start();
     });
-        
 
     $("#btnStop").click(function(){
         stop();
     });
-
-}
-
-function start(){
-    if(timerId == 0){
-        timerId = setInterval(function(){
-            moveFish();
-        }, 100);
-    }
-}
-
-function stop() {
-    clearInterval(timerId);
-    timerId = 0;
-}
-
-function moveFish(){
-    var x = fish.position().left + step;
-
-    if(x >= 430) {
-        x = 430;
-        fish.attr("src", "fish2.png");
-        step -= 50;
-    }else if(x <= 50) {
-        x = 50;
-        fish.attr("src", "fish1.png");
-        step += 50;
-    }
-
-    fish.css("left", x);
 }
