aabd8910a16407197f16e68fd47f9f361dd1312b
dohyun8736 <dohyun8736@naver.com>
2018-10-01 00:03:15 +0900

    timer misson3 clear

---
+++ javascript+jquery 완전정복 스터디1/409p timer misson/misson3/main.js
@@ -0,0 +1,48 @@
+var banner = null;
+var timerID = false;
+var bannerArray = [];
+var imgLength = 9;
+var count = 0;
+
+$(document).ready(function(){
+    bannerInit();
+    bannerEvent();
+});
+
+function bannerInit(){
+    banner = $("#banner1");
+    btnStart = $("#btnStart");
+    btnStop = $("#btnStop");
+    for(var i = 1; i < imgLength; i++){
+        bannerArray.push("images/logo_0"+ i +".jpg");
+    }
+}
+
+function bannerEvent() {
+    btnStart.click(function(){
+        start();
+    });
+
+    btnStop.click(function(){
+        stop();
+    });
+}
+
+function start(){
+    if(timerID == false){
+        timerID = setInterval(bannerMove, 1000);
+    }
+}
+
+function stop(){
+    clearInterval(timerID);
+    timerID = 0;
+}
+
+function bannerMove(){
+    banner.attr("src", bannerArray[count]);
+    count++;
+    if(count == imgLength - 1){
+        count = 0;
+    }
+}
