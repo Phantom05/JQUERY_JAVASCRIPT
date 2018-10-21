804e2b5318871d572c82abf5d87cf3060a0df603
dohyun8736 <dohyun8736@naver.com>
2018-10-22 01:09:24 +0900

    새벽 공부 끝

---
+++ javascript+jquert 완전정복 스터디2/78p jquery 일반노드 찾기/아이디 이름으로 노드 찾기/main.js
@@ -0,0 +1,9 @@
+// 문서에서 id가 header인 노드를 찾가 border 속성을 "4px solid #f00" 으로 변경하기
+
+
+$(document).ready(function(){
+    var header = $("#header");
+    header.css("border" , "4px solid #ff0");
+});
+
+
